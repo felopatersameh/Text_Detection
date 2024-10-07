4a5ca6a23ee306fdd3007bd9c184b306e68ac49b
OM AR <om1446587@gmail.com>
2024-10-07 21:46:44 +0300

    3b5ale2

--- lib/features/intro/view/pages/intro_screen.dart
+++ lib/features/intro/view/pages/intro_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
 import 'package:textdetection/features/auth/view/pages/login_screen.dart';
 import 'package:textdetection/features/intro/view/widgets/intro_custom.dart';
 import 'package:textdetection/generated/assets.dart';
+
 import '../../../../core/constant/colors.dart';
 
 class IntroScreen extends StatelessWidget {
@@ -41,30 +42,34 @@ class IntroScreen extends StatelessWidget {
       headerBackgroundColor: AppColors.cream,
       pageBackgroundColor: AppColors.white,
       background: [
-        SizedBox(width: 0,),
-        SizedBox(width: 0,),
-        SizedBox(width: 0,),
+        SizedBox(
+          width: 0,
+        ),
+        SizedBox(
+          width: 0,
+        ),
+        SizedBox(
+          width: 0,
+        ),
       ],
       speed: 1.8,
       pageBodies: [
         IntroCustomBody(
-            imageUrl: Assets.pngWelcome1,
-            title: 'Welcome to Quizzo!',
-            subtitle: 'Compete with friends,'
-                'earn points, and climb the leaderboard in this addictive trivia challenge.',
+          imageUrl: Assets.pngWelcome1,
+          title: 'Welcome to Quizzo!',
+          subtitle: 'Compete with friends,'
+              'earn points, and climb the leaderboard in this addictive trivia challenge.',
         ),
         IntroCustomBody(
             imageUrl: Assets.pngWelcome2,
             title: "The Ultimate Trivia Challenge",
             subtitle: "Put your knowledge to the test and prove "
-                "your expertise across a wide range of topics in this engaging game."
-        ),
+                "your expertise across a wide range of topics in this engaging game."),
         IntroCustomBody(
             imageUrl: Assets.pngWelcome3,
             title: "Test Your Knowledge with Quizzo",
             subtitle: "Quizzo is the perfect app to challenge "
-                "yourself and your friends, with endless trivia fun at your fingertips."
-        ),
+                "yourself and your friends, with endless trivia fun at your fingertips."),
       ],
     );
   }
