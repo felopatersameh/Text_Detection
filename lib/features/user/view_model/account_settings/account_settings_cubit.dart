3e0037f68e7523539a7a66f0f55953a951cb26e9
kareem <kareemwael9898@gmail.com>
2024-10-13 00:40:16 +0300

    Updated Cubit Files

--- lib/features/user/view_model/account_settings/account_settings_cubit.dart
+++ lib/features/user/view_model/account_settings/account_settings_cubit.dart
@@ -1,9 +1,29 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:textdetection/features/user/model/user_data_model.dart';
 
 part 'account_settings_state.dart';
 
 class AccountSettingsCubit extends Cubit<AccountSettingsState> {
   AccountSettingsCubit() : super(AccountSettingsInitial());
 
+  void changeUserName(String newName) {
+    UserDataModel.name = newName;
+    emit(UserNameChanged());
+  }
+
+  void changeEmail(String newEmail) {
+    UserDataModel.email = newEmail;
+    emit(EmailChanged());
+  }
+
+  void changePassword(String newPassword) {
+    UserDataModel.password = newPassword;
+    emit(PasswordChanged());
+  }
+
+  void changeImage(String newImageUrl) {
+    UserDataModel.imageUrl = newImageUrl;
+    emit(ImageChanged());
+  }
 }
--- lib/features/user/view_model/account_settings/account_settings_state.dart
+++ lib/features/user/view_model/account_settings/account_settings_state.dart
@@ -4,3 +4,11 @@ part of 'account_settings_cubit.dart';
 sealed class AccountSettingsState {}
 
 final class AccountSettingsInitial extends AccountSettingsState {}
+
+final class UserNameChanged extends AccountSettingsState {}
+
+final class EmailChanged extends AccountSettingsState {}
+
+final class PasswordChanged extends AccountSettingsState {}
+
+final class ImageChanged extends AccountSettingsState {}
