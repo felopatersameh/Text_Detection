4d1ef444c2d2bc32ac3073afe2fd55bba361082f
OM AR <om1446587@gmail.com>
2024-10-16 03:23:14 +0300

    strings refactor (Adding Comments)

--- lib/core/constant/string.dart
+++ lib/core/constant/string.dart
@@ -1,40 +1,68 @@
 class AppString {
   static const String appName = 'Text Detector App';
-
   //--------------------------------------------------
+  //AUTH => login screen
+  static const String loginNow = "Login Now";
   static const String loginHeader = "Welcome back! Glad to see you, Again!";
   static const String loginName = "Login";
+  static const String dontHaveAnAccount = "Don’t have an account? ";
+  static const String orLoginWith = "Or Login with";
+
+  //--------------------------------------------------
+  //AUTH => register screen
   static const String registerName = "Register";
   static const String registerHeader = "Hello! Register to get started";
   static const String passwordHint = 'Enter your password';
   static const String confirmPasswordHint = 'Confirm your password';
   static const String phoneHint = 'Enter Phone no.';
   static const String userNameHint = 'Username';
-  static const String emailHint = 'Email or Phone';
-  static const String forgotPassword = "Forgot Password?";
-  static const String forgotPasswordMassage = "Don't worry! It occurs. Please enter the email address linked with your account.";
-  static const String dontHaveAnAccount = "Don’t have an account? ";
   static const String registerNow = "Register Now";
   static const String haveAccount = "Already have an account? ";
-  static const String loginNow = "Login Now";
-  static const String verifyOtpCode = "Verify";
-  static const String orLoginWith = "Or Login with";
   static const String orRegisterWith = "Or Register with";
-  static const String didntReceivedAnyOtpCode = "Didn’t received code?";
+
+  //--------------------------------------------------
+  //AUTH => forgotPassword screen
+  static const String emailHint = 'Email or Phone';
+  static const String forgotPassword = "Forgot Password?";
+  static const String forgotPasswordMassage =
+      "Don't worry! It occurs. Please enter the email address linked with your account.";
+
+  //--------------------------------------------------
+  //AUTH => otp screen
+  static const String verifyOtpCode = "Verify";
   static const String resendOtpCode = "Resend";
   static const String otpMessage = "OTP Verification";
   static const String enterYourOTP =
       "Enter the verification code we just sent on your email address or phone number.";
+  static const String didntReceivedAnyOtpCode = "Didn’t received code?";
+  static const String sendCode = "Send Code";
+
+  //--------------------------------------------------
+  //AUTH => createNewPassword screen
   static const String createNewPasswordHeader = "Creare New Password";
   static const String createNewPasswordMessage =
       "Your new password must be unique from those previously used.";
   static const String newPasswordHint = "New Password";
   static const String resetPasswordHint = "Reset Password";
-  static const String sendCode = "Send Code";
+
+  //--------------------------------------------------
+  //AUTH => BodyForgetPassword screen
   static const String rememberPassword = "Remember Password? ";
+
+  //--------------------------------------------------
+  //AUTH => ChangedPassword screen
   static const String changedPasswordHeader = "Password Changed!";
-  static const String changedPasswordHint = "Your password has been changed successfully.";
+  static const String changedPasswordHint =
+      "Your password has been changed successfully.";
   static const String backToLogIN = "Back to Login";
+
+  //--------------------------------------------------
+  //Me =>user Screen
   static const String extractedTextHeader = "Extracted Text";
+
+  //--------------------------------------------------
+  //Me =>user Screen
   static const String accountSettingsHeader = "Account Settings";
+//--------------------------------------------------
+//Todo: اللي يضيف أي سترينج يميزه بكومنت بعد اذنكم
 }
