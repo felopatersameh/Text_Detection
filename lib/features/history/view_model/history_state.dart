5e37f41569ae729c3a41393507fc6e03cae54648
kareem <kareemwael9898@gmail.com>
2024-10-17 16:51:27 +0300

    Updated History Cubit

--- lib/features/history/view_model/history_state.dart
+++ lib/features/history/view_model/history_state.dart
@@ -4,3 +4,9 @@ part of 'history_cubit.dart';
 sealed class HistoryState {}
 
 final class HistoryInitial extends HistoryState {}
+
+final class AddedFile extends HistoryState {}
+
+final class RemovedFile extends HistoryState {}
+
+final class ClearHistory extends HistoryState {}
