d00812dcdd1413e6281867c1c7cbca4376e1ef8b
kareem <kareemwael9898@gmail.com>
2024-10-17 17:45:54 +0300

    Improved settings_page.dart

--- lib/features/user/view/pages/settings_page.dart
+++ lib/features/user/view/pages/settings_page.dart
@@ -9,10 +9,11 @@ import 'package:textdetection/core/utils/Widget/build_default_button.dart';
 import 'package:textdetection/features/user/view_model/account_settings/account_settings_cubit.dart';
 
 import '../../../../core/constant/assets.dart';
-import '../../../../core/constant/colors.dart';
+import '../../../../core/utils/Widget/custom_list_tile.dart';
 import '../../../../core/utils/Widget/password_text__form_field.dart';
 import '../../../auth/view/widgets/back_arrow.dart';
 import '../../../auth/view/widgets/custom_text_form_field.dart';
+import '../widgets/custom_expansion_tile.dart';
 
 class AccountSettingsScreen extends StatefulWidget {
   const AccountSettingsScreen({super.key});
@@ -46,84 +47,54 @@ class _AccountSettingsScreenState extends State<AccountSettingsScreen> {
               padding: EdgeInsets.symmetric(horizontal: 40.w),
               children: [
                 60.h.verticalSpace,
-                Material(
-                  elevation: 4.w,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(6.w),
-                  ),
-                  child: ExpansionTile(
-                    title: Text(
-                      "Change User Name",
-                    ),
-                    subtitle: Text(
-                      // UserDataModel.name,
-                      context.watch<AccountSettingsCubit>().state.name,
-                      style: TextStyle(color: AppColors.mediumGray),
-                    ),
-                    backgroundColor: Colors.amberAccent,
-                    collapsedBackgroundColor: Colors.white,
-                    leading: SvgPicture.asset(AppAssets.accountEmail),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(6.w)),
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 12),
-                        child: Form(
-                          autovalidateMode: AutovalidateMode.onUnfocus,
-                          child: CustomTextFormField(
-                            hint: "Enter Your Name",
-                            controller: _nameController,
-                            validator: (p0) {
-                              return customValidate(value: p0.toString());
-                            },
-                          ),
+                CustomExpansionTile(
+                  title: "Change User Name",
+                  subtitle: context.watch<AccountSettingsCubit>().state.name,
+                  backgroundColor: Colors.amberAccent,
+                  collapsedBackgroundColor: Colors.white,
+                  leading: SvgPicture.asset(AppAssets.accountEmail),
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 12),
+                      child: Form(
+                        autovalidateMode: AutovalidateMode.onUnfocus,
+                        child: CustomTextFormField(
+                          hint: "Enter Your Name",
+                          controller: _nameController,
+                          validator: (p0) {
+                            return customValidate(value: p0.toString());
+                          },
                         ),
-                      )
-                    ],
-                  ),
+                      ),
+                    )
+                  ],
                 ),
-                30.h.verticalSpace,
-                Material(
-                  elevation: 4.w,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(6.w)),
-                  child: ExpansionTile(
-                    title: Text(
-                      "Change Password",
-                    ),
-                    backgroundColor: Colors.orangeAccent,
-                    collapsedBackgroundColor: Colors.white,
-                    leading: Icon(Icons.lock),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(6.w)),
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 12.w),
-                        child: Form(
-                          key: _passFormKey,
-                          autovalidateMode: AutovalidateMode.onUnfocus,
-                          child: CustomPasswordTextFromField(
-                            controller: _passwordController,
-                            fieldId: "settingsPass",
-                          ),
+                30.dm.verticalSpace,
+                CustomExpansionTile(
+                  title: "Change Password",
+                  backgroundColor: Colors.orangeAccent,
+                  collapsedBackgroundColor: Colors.white,
+                  leading: Icon(Icons.lock),
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 12.w),
+                      child: Form(
+                        key: _passFormKey,
+                        autovalidateMode: AutovalidateMode.onUnfocus,
+                        child: CustomPasswordTextFromField(
+                          controller: _passwordController,
+                          fieldId: "settingsPass",
                         ),
-                      )
-                    ],
-                  ),
+                      ),
+                    )
+                  ],
                 ),
-                30.h.verticalSpace,
-                Material(
-                  elevation: 4.w,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(6.w)),
-                  child: ListTile(
-                    title: Text("Change Profile Image"),
-                    tileColor: Colors.white,
-                    leading: Icon(Icons.image_outlined),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(6.w)),
-                    onTap: () {},
-                  ),
+                30.dm.verticalSpace,
+                CustomListTile(
+                  title: "Change Profile Image",
+                  tileColor: Colors.white,
+                  leading: Icon(Icons.image_outlined),
+                  onTap: () {},
                 ),
                 30.h.verticalSpace,
               ],
